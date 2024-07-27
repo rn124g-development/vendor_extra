91b36ffaf99ee2b81c483a6849c0e4ffb86d9698
Ali Hasan <ahb7671@gmail.com>
2024-07-27 10:28:08 +0500

    include signing keys
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- product.mk
+++ product.mk
@@ -11,3 +11,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc
+
+# keys
+include certs/keys.mk
+
