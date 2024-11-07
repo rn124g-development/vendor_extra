6b1b809107c4cc60607f4456b029d09a7d384e1b
Ali Hasan <ahb7671@gmail.com>
2024-11-07 14:12:38 +0500

    extras: Add error message if certs/keys.mk is missing
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- product.mk
+++ product.mk
@@ -12,5 +12,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/feather/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc
 
-# keys
+# Check if certs/keys.mk exists
+ifneq ("$(wildcard certs/keys.mk)", "")
 include certs/keys.mk
+else
+$(error Private signing keys do not exist. If this is intentional, remove or comment this include)
+endif
+
