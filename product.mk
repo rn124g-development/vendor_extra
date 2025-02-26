f3d935d9cc1cea948c59f1db83cea0eb296472dc
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-26 21:37:22 +0800

    Disable art debug builds

--- product.mk
+++ product.mk
@@ -12,6 +12,13 @@ PRODUCT_PACKAGES += \
 #PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc
 
+# Disable debugging
+ART_BUILD_TARGET_NDEBUG := false
+ART_BUILD_TARGET_DEBUG := false
+ART_BUILD_HOST_NDEBUG := false
+ART_BUILD_HOST_DEBUG := false
+USE_DEX2OAT_DEBUG := false
+
 # Check if certs/keys.mk exists
 ifneq ("$(wildcard certs/keys.mk)", "")
 include certs/keys.mk
